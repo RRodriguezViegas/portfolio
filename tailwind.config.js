3d468cda24bea4b4058c55ef43cebfb36c137a1b
RRodriguezViegas <ramiirv26@gmail.com>
2024-12-15 14:39:37 -0300

    add breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+      "2xl": "1536px",
+      "3xl": "1920px",
+    },
   },
   plugins: [],
 };
