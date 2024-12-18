182b7f29f1c011711ea9f8fd822c17f32147cdb6
RRodriguezViegas <ramiirv26@gmail.com>
2024-12-18 14:00:55 -0300

    implemented EmailJS to send me an email with info from the form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
   const [hora, setHora] = useState("");
+  const form = useRef();
 
   useEffect(() => {
     const actualizarHora = () => {
@@ -20,6 +22,29 @@ function Contact() {
 
     return () => clearInterval(intervalo);
   }, []);
+
+  const sendEmail = e => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_brfgrgt", // Reemplazá con tu Service ID
+        "template_0h91mk9", // Reemplazá con tu Template ID
+        form.current,
+        "Kn3eZFFfhy85sc4QJ" // Reemplazá con tu Public Key
+      )
+      .then(
+        result => {
+          console.log("Email enviado:", result.text);
+          alert("¡Email enviado con éxito!");
+        },
+        error => {
+          console.log("Error al enviar:", error.text);
+          alert("Hubo un error, intentá de nuevo.");
+        }
+      );
+  };
+
   return (
     <div className="section h-screen flex items-center justify-center text-[#242038]">
       <div className="w-3/4 h-3/6 flex flexs-row">
@@ -42,8 +67,7 @@ function Contact() {
         </section>
         <section className="w-1/2 h-3/4  flex flex-col justify-between">
           <h1 className="text-2xl pb-4">Say hello</h1>
-          {/* //! Alinear con el renglon de Let's collaborate */}
-          <form action="">
+          <form onSubmit={sendEmail} ref={form}>
             <div className="grid grid-cols-2 gap-4">
               <div>
                 <label htmlFor="name" className="text-xs">
@@ -52,8 +76,10 @@ function Contact() {
                 <br />
                 <input
                   type="text"
+                  name="name"
                   className="text-lg w-full bg-transparent border-b-2 border-transparent focus:outline-none focus:border-gray-800 placeholder-[#2420387a] focus:placeholder-transparent transition-all duration-300"
                   placeholder="Your name"
+                  required
                 />
               </div>
               <div>
@@ -63,8 +89,10 @@ function Contact() {
                 <br />
                 <input
                   type="text"
+                  name="subject"
                   className="text-lg w-full bg-transparent border-b-2 border-transparent focus:outline-none focus:border-gray-800 placeholder-[#2420387a] focus:placeholder-transparent transition-all duration-300"
                   placeholder="Choose subject"
+                  required
                 />
               </div>
               <div>
@@ -74,6 +102,7 @@ function Contact() {
                 <br />
                 <input
                   type="text"
+                  name="company"
                   className="text-lg w-full bg-transparent border-b-2 border-transparent focus:outline-none focus:border-gray-800 placeholder-[#2420387a] focus:placeholder-transparent transition-all duration-300"
                   placeholder="Your company"
                 />
@@ -84,9 +113,11 @@ function Contact() {
                 </label>
                 <br />
                 <input
-                  type="text"
+                  type="email"
+                  name="user_email"
                   className="text-lg w-full bg-transparent border-b-2 border-transparent focus:outline-none focus:border-gray-800 placeholder-[#2420387a] focus:placeholder-transparent transition-all duration-300"
                   placeholder="Email address"
+                  required
                 />
               </div>
               <div>
@@ -94,17 +125,22 @@ function Contact() {
                   MESSAGE
                 </label>
                 <br />
-                <input
+                <textarea
                   type="text"
-                  className="text-lg w-full bg-transparent border-b-2 border-transparent focus:outline-none focus:border-gray-800 placeholder-[#2420387a] focus:placeholder-transparent transition-all duration-300"
+                  name="message"
+                  className="text-lg w-full bg-transparent border border-transparent focus:outline-none focus:border-[#242038] placeholder-[#2420387a] focus:placeholder-transparent transition-all duration-300"
                   placeholder="Start typing here"
+                  required
                 />
               </div>
             </div>
+            <button
+              type="submit"
+              className="underline text-xl inline-block self-start"
+            >
+              Submit &rarr;
+            </button>
           </form>
-          <button className="underline text-xl inline-block self-start">
-            Submit &rarr;
-          </button>
         </section>
       </div>
     </div>
