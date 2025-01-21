42c7d812010344a96ff68f1b08f307c4eb0cc907
RRodriguezViegas <ramiirv26@gmail.com>
2025-01-21 15:29:54 -0300

    Pointer so it matches the link cursor

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -48,7 +48,7 @@ function Projects() {
           <div className="relative w-60 h-60 rounded-sm flex justify-center items-center shadow-2xl hover:scale-110 hover:-translate-y-6 ease-in-out duration-300">
             <div className="absolute w-full h-full flex justify-center items-center bg-[url('src/assets/rama-portfolio-bg.png')] bg-contain blur-sm" />
             <div className="relative w-1/2 h-1/2 rounded-sm">
-              <span className="text-9xl text-[#242038] MuseoModerno cursor-default">
+              <span className="text-9xl text-[#242038] MuseoModerno cursor-pointer">
                 R
               </span>
             </div>
