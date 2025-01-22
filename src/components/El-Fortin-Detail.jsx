1c49015b4b0617802609d45060c7ef05d09e3013
RRodriguezViegas <ramiirv26@gmail.com>
2025-01-22 10:01:59 -0300

    test

--- src/components/El-Fortin-Detail.jsx
+++ src/components/El-Fortin-Detail.jsx
@@ -4,9 +4,7 @@ function ElFortinDetail() {
   return (
     <>
       <div className="bg-[#F1D1C1] h-screen flex items-center justify-center">
-        <h1 className="text-5xl font-bold text-[#242038]">
-          El Fortin lo cambio por cualquier cosa
-        </h1>
+        <h1 className="text-5xl font-bold text-[#242038]">El Fortin test</h1>
       </div>
     </>
   );
