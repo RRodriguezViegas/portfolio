9c2c2d084cb088afc76ec685f4fdcf8c7d0684a1
RRodriguezViegas <ramiirv26@gmail.com>
2025-03-03 12:11:46 -0300

    responsive wip

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -102,16 +102,16 @@ function Contact() {
       /> */}
 
       <div
-        className="ct-appear w-3/4 h-5/6 lg:h-3/6 flex flex-col lg:flex-row rounded-xlp-2"
+        className="ct-appear w-3/4 h-5/6 lg:h-3/6 flex flex-col lg:flex-row"
         style={{ willChange: "filter, transform" }}
       >
-        <section className="w-full lg:w-1/2 h-3/4 mb-8 lg:mb-0 flex flex-col justify-between">
+        <section className="w-full xl:w-1/2 h-3/4 mb-8 lg:mb-0 flex flex-col justify-between">
           <div>
             <h1 className="MuseoModerno text-7xl pb-5">Contacto</h1>
             <h3 className="text-xl 2xl:text-2xl">Buenos Aires, Argentina</h3>
             <p className="text-lg 2xl:text-xl font-light">{hora}</p>
           </div>
-          <div className="flex flex-row justify-between text-xl 2xl:text-2xl mr-28">
+          <div className="flex flex-col sm:flex-row justify-between text-xl 2xl:text-2xl xl:mr-28">
             <div>
               <p>Enviame un mail</p>
               <p className="font-light text-lg 2xl:text-xl">
@@ -126,7 +126,7 @@ function Contact() {
             </div>
           </div>
         </section>
-        <section className="w-full lg:w-1/2 h-3/4  flex flex-col justify-between">
+        <section className="w-full xl:w-1/2 h-3/4  flex flex-col justify-between">
           <h1 className="text-2xl pb-4">no lo sé</h1>
           <form onSubmit={sendEmail} ref={form}>
             <div className="grid grid-cols-2 gap-4">
