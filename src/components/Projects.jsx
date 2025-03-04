7c6bc9fb2b40178ccaf9f06231c80d5b043b3d59
RRodriguezViegas <ramiirv26@gmail.com>
2025-03-04 12:06:09 -0300

    mejora de bordes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 
 function Projects() {
   return (
-    <div className="about-bg pt-10 section h-full lg:h-screen flex flex-col items-center justify-center text-center">
+    <div className="about-bg pt-10 section h-full lg:h-screen flex flex-col items-center justify-center md:text-center mx-4 md:mx-0">
       <h1 className="pr-appear MuseoModerno text-4xl lg:text-7xl font-bold text-[#242038]">
         Algunos de mis proyectos
       </h1>
-      <p className="pr-appear text-xl lg:text-2xl text-[#242038] p-4">
+      <p className="pr-appear text-xl lg:text-2xl text-[#242038] py-4">
         Una selección de proyectos que muestran mis habilidades como
         desarrollador
       </p>
 
-      <div className="pr-appear grid grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-10 p-4 w-[80vw] max-w-[900px]">
+      <div className="pr-appear grid grid-cols-2 lg:grid-cols-3 gap-4 md:gap-10 sm:p-4 sm:w-[80vw] max-w-[900px]">
         <Link to={"/elfortin"}>
           <div className="relative aspect-square rounded-sm flex justify-center items-center shadow-2xl lg:hover:scale-110 lg:hover:-translate-y-6 ease-in-out duration-300">
             <div
