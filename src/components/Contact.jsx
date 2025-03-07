f9609f0bf37e1683c46099cd6e96f1731d2f9af1
RRodriguezViegas <ramiirv26@gmail.com>
2025-03-07 12:49:37 -0300

    chau

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -46,113 +46,124 @@ function Contact() {
   };
 
   return (
-    // <div className="about-bg z-0 relative section h-screen my-20 flex items-center justify-center text-[#242038]">
-    //   <div className="ct-appear mx-4 sm:mx-0 w-full sm:w-3/4 h-full space-y-12 lg:h-3/6 flex flex-col lg:flex-row">
-    //     <section className="w-full xl:w-1/2 h-full mb-12 lg:mb-0 flex flex-col justify-between">
-    //       <div>
-    //         <h1 className="MuseoModerno text-4xl lg:text-7xl pb-5">Contacto</h1>
-    //         <h3 className="text-xl 2xl:text-2xl">Buenos Aires, Argentina</h3>
-    //         <p className="text-lg 2xl:text-xl font-light">{hora}</p>
-    //       </div>
-    //       <div className="flex flex-col space-y-4 sm:space-y-0 sm:flex-row justify-between text-xl 2xl:text-2xl lg:mr-4 xl:mr-28">
-    //         <div>
-    //           <p>Enviame un mail</p>
-    //           <p className="font-light text-lg 2xl:text-xl">
-    //             ramirorvdev@gmail.com
-    //           </p>
-    //         </div>
-    //         <div>
-    //           <p>O si preferis charlar</p>
-    //           <p className="font-light text-lg 2xl:text-xl">
-    //             +54 9 11 4146 1834
-    //           </p>
-    //         </div>
-    //       </div>
-    //     </section>
-    //     <section className="w-full xl:w-1/2 h-full flex flex-col justify-between">
-    //       <h1 className="text-2xl pb-4">Enviame un mensaje</h1>
-    //       <form onSubmit={sendEmail} ref={form}>
-    //         <div className="grid grid-cols-2 gap-4">
-    //           <div>
-    //             <label htmlFor="name" className="text-md">
-    //               NOMBRE
-    //             </label>
-    //             <br />
-    //             <input
-    //               type="text"
-    //               name="name"
-    //               className="text-lg w-full bg-transparent border-b-2 border-transparent focus:outline-none focus:border-gray-800 placeholder-[#2420387a] focus:placeholder-transparent transition-all duration-300"
-    //               placeholder="Tu nombre"
-    //               required
-    //             />
-    //           </div>
-    //           <div>
-    //             <label htmlFor="name" className="text-md">
-    //               ASUNTO
-    //             </label>
-    //             <br />
-    //             <input
-    //               type="text"
-    //               name="subject"
-    //               className="text-lg w-full bg-transparent border-b-2 border-transparent focus:outline-none focus:border-gray-800 placeholder-[#2420387a] focus:placeholder-transparent transition-all duration-300"
-    //               placeholder="Motivo de consulta"
-    //               required
-    //             />
-    //           </div>
-    //           <div>
-    //             <label htmlFor="name" className="text-md">
-    //               EMPRESA
-    //             </label>
-    //             <br />
-    //             <input
-    //               type="text"
-    //               name="company"
-    //               className="text-lg w-full bg-transparent border-b-2 border-transparent focus:outline-none focus:border-gray-800 placeholder-[#2420387a] focus:placeholder-transparent transition-all duration-300"
-    //               placeholder="Tu empresa"
-    //             />
-    //           </div>
-    //           <div>
-    //             <label htmlFor="name" className="text-md">
-    //               EMAIL
-    //             </label>
-    //             <br />
-    //             <input
-    //               type="email"
-    //               name="user_email"
-    //               className="text-lg w-full bg-transparent border-b-2 border-transparent focus:outline-none focus:border-gray-800 placeholder-[#2420387a] focus:placeholder-transparent transition-all duration-300"
-    //               placeholder="Tu correo electrónico"
-    //               required
-    //             />
-    //           </div>
-    //           <div>
-    //             <label htmlFor="name" className="text-md">
-    //               MENSAJE
-    //             </label>
-    //             <br />
-    //             <textarea
-    //               type="text"
-    //               name="message"
-    //               className="text-lg w-full bg-transparent border border-transparent focus:outline-none focus:border-[#242038] placeholder-[#2420387a] focus:placeholder-transparent transition-all duration-300"
-    //               placeholder="Escribí acá"
-    //               required
-    //             />
-    //           </div>
-    //         </div>
-    //         <button
-    //           type="submit"
-    //           className="underline text-xl inline-block self-start"
-    //         >
-    //           Enviar &rarr;
-    //         </button>
-    //       </form>
-    //     </section>
-    //   </div>
-    // </div>
-    <div className="section h-screen my-20 flex items-center justify-center text-[#242038]">
-      <section className="w-3/4 h-3/4 bg-gray-100 rounded-md flex items-center justify-center">
-        <section className="w-5/12 h-full bg-blue-500 rounded-l-md custom-shadow"></section>
-        <section className="w-7/12 h-full rounded-r-md "></section>
-      </section>
+    <div className="section fonts h-full xl:h-screen my-20 flex items-center justify-center text-[#242038]">
+      <div className="absolute contact-bg blur-3xl w-full h-full" />
+      <div className="relative w-3/4 h-5/6 flex flex-col xl:flex-row ">
+        <section className="relative w-full xl:w-1/2 h-full pr-12">
+          <span className="text-7xl text-[#00075A] font-bold block">
+            Contame sobre tu proyecto
+          </span>
+          <span className="text-lg font-medium block mt-10 mb-20">
+            Cada proyecto es único, y quiero asegurarme de que el tuyo tenga
+            todo lo que necesitás. Así es como trabajaremos juntos para
+            lograrlo:
+          </span>
+          <div>
+            <div className="relative border-l-2 border-[#5921B9]">
+              <svg
+                width="22"
+                height="22"
+                viewBox="0 0 22 22"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="absolute -left-3 -top-5 mr-2"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M11 22C17.0751 22 22 17.0751 22 11C22 4.92487 17.0751 0 11 0C4.92487 0 0 4.92487 0 11C0 17.0751 4.92487 22 11 22ZM11 15C13.2091 15 15 13.2091 15 11C15 8.79086 13.2091 7 11 7C8.79086 7 7 8.79086 7 11C7 13.2091 8.79086 15 11 15Z"
+                  fill="#133695"
+                />
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M11 18C14.866 18 18 14.866 18 11C18 7.13401 14.866 4 11 4C7.13401 4 4 7.13401 4 11C4 14.866 7.13401 18 11 18ZM11 15C13.2091 15 15 13.2091 15 11C15 8.79086 13.2091 7 11 7C8.79086 7 7 8.79086 7 11C7 13.2091 8.79086 15 11 15Z"
+                  fill="#5921B9"
+                />
+              </svg>
+              <span className="absolute left-8 -top-6 text-xl font-semibold text-[#00075A]">
+                1. Te envío una propuesta
+              </span>
+              <span className="text-lg font-light block px-8 pt-4 pb-10">
+                Con la información que me des en el formulario, analizo qué
+                necesitás y te armo una propuesta inicial. Esto incluye ideas de
+                diseño, funcionalidades y una estimación de tiempo y costo.
+              </span>
+            </div>
+
+            <div className="relative border-l-2 border-[#5921B9]">
+              <svg
+                width="22"
+                height="22"
+                viewBox="0 0 22 22"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="absolute -left-3 mr-2"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M11 22C17.0751 22 22 17.0751 22 11C22 4.92487 17.0751 0 11 0C4.92487 0 0 4.92487 0 11C0 17.0751 4.92487 22 11 22ZM11 15C13.2091 15 15 13.2091 15 11C15 8.79086 13.2091 7 11 7C8.79086 7 7 8.79086 7 11C7 13.2091 8.79086 15 11 15Z"
+                  fill="#133695"
+                />
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M11 18C14.866 18 18 14.866 18 11C18 7.13401 14.866 4 11 4C7.13401 4 4 7.13401 4 11C4 14.866 7.13401 18 11 18ZM11 15C13.2091 15 15 13.2091 15 11C15 8.79086 13.2091 7 11 7C8.79086 7 7 8.79086 7 11C7 13.2091 8.79086 15 11 15Z"
+                  fill="#5921B9"
+                />
+              </svg>
+              <span className="text-xl font-semibold px-8 text-[#00075A]">
+                2. Ajustamos juntos los detalles
+              </span>
+              <span className="text-lg font-light block px-8 pt-3 pb-10">
+                Te comparto la propuesta y charlamos para hacer los cambios
+                necesarios. Podés contarme si hay algo que te gustaría agregar o
+                modificar hasta que lleguemos a la mejor versión para vos.
+              </span>
+            </div>
+
+            <div className="relative border-l-2 border-[#ffffff00]">
+              <svg
+                width="22"
+                height="22"
+                viewBox="0 0 22 22"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="absolute -left-3 mr-2"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M11 22C17.0751 22 22 17.0751 22 11C22 4.92487 17.0751 0 11 0C4.92487 0 0 4.92487 0 11C0 17.0751 4.92487 22 11 22ZM11 15C13.2091 15 15 13.2091 15 11C15 8.79086 13.2091 7 11 7C8.79086 7 7 8.79086 7 11C7 13.2091 8.79086 15 11 15Z"
+                  fill="#133695"
+                />
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M11 18C14.866 18 18 14.866 18 11C18 7.13401 14.866 4 11 4C7.13401 4 4 7.13401 4 11C4 14.866 7.13401 18 11 18ZM11 15C13.2091 15 15 13.2091 15 11C15 8.79086 13.2091 7 11 7C8.79086 7 7 8.79086 7 11C7 13.2091 8.79086 15 11 15Z"
+                  fill="#5921B9"
+                />
+              </svg>
+              <span className="text-xl font-semibold px-8 text-[#00075A]">
+                3. Empezamos con el proyecto
+              </span>
+              <span className="text-lg font-light block px-8 pt-3">
+                Una vez que todo esté definido, pongo manos a la obra. Durante
+                el proceso te voy mostrando avances y ajustes para que el
+                resultado sea exactamente lo que esperás.
+              </span>
+            </div>
+          </div>
+        </section>
+        <section className="w-full xl:w-1/2 h-full custom-shadow bg-white rounded-xl p-12">
+          <span className="text-lg font-medium">
+            Completá el formulario y en breve te respondo con una propuesta
+            personalizada.{" "}
+            <span className="font-semibold">¡El presupuesto es sin cargo!</span>
+          </span>
+        </section>
+      </div>
     </div>
   );
 }
